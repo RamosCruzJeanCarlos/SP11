79cc67eb8fe8fc368dc1381bbdea618d1c00dd2d
yonkoujean <yonkoujean@gmail.com>
2017-07-08 15:18:00 -0500

    Update Content SCAE.bat

--- Release/FISI/rollback/SCAE.bat
+++ Release/FISI/rollback/SCAE.bat
@@ -1,1 +1,15 @@
-tree %1 /f /a >C:\Users\G62\Desktop\SCAE.txt
+@echo off
+COPY "..\Libreria de trabajo\Documentos\Formato de pase a produccion.xlsx" "Formato de pase a produccion.xlsx"
+set NOMBRE_PROYECTO=SCAE
+set FECHA_ACTUAL=%DATE%
+set ANIO=%FECHA_ACTUAL:~6,4%
+set MES=%FECHA_ACTUAL:~3,2%
+set DIA=%FECHA_ACTUAL:~0,2%
+set RUTA=%NOMBRE_PROYECTO%_%DIA%%MES%%ANIO%
+MD %RUTA%
+cd %RUTA% 
+MD Documentos
+MD Scripts
+MD Ejecutable
+MD Rollback
+exit
